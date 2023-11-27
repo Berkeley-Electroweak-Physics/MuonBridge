08859676ab4f84b3aedc43570cc5e045f902978f
Kenneth S. McElvain <kenmcelvain@me.com>
2023-11-26 16:48:50 -0800

    Update setupbridge.sh

--- setupbridge.sh
+++ setupbridge.sh
@@ -13,9 +13,19 @@ topd=`pwd`
 echo "Top directory is " ${topd}
 
 # names of repositories we want to clone
-mu2e=Mu2e_Internal # will change to Mu2e
+muonconverter=MuonConverter
+mu2e=Mu2e_NRET
 elastic=Elastic
 
+# Repository for SMEFT -> Mu2e_NRET
+if [ ! -d ${muonconverter} ]
+then
+	echo git clone git@github.com:Berkeley-Electroweak-Physics/${muonconverter}.git
+	git clone git@github.com:Berkeley-Electroweak-Physics/${muonconverter}.git
+else
+	echo ../${muonconverter} " already exists. Use git pull to get changes if needed. "
+fi
+# Nucleon/nucleus level EFT for Muon to electron conversion
 if [ ! -d ${mu2e} ]
 then
 	echo git clone git@github.com:Berkeley-Electroweak-Physics/${mu2e}.git
@@ -23,6 +33,7 @@ then
 else
 	echo ../${mu2e} " already exists. Use git pull to get changes if needed. "
 fi
+# One-body density matrices for target nuclei
 if [ ! -d ${elastic} ]
 then
 	echo git clone git@github.com:Berkeley-Electroweak-Physics/${elastic}.git
